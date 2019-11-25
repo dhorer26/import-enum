d2c9aa7b8104971a7f500aac88a53f7f356bab27
Ravindra Dhore <ravindrad@ansh.com>
2019-11-25 15:51:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DisplaySize } from './enum/display-size.enum';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'enum-usage';
+  displaySize: DisplaySize = DisplaySize.MEDIUM;
 }
---
+++ src/app/enum/display-size.enum.ts
@@ -0,0 +1,5 @@
+export const enum DisplaySize {
+  SMALL = 'sm',
+  MEDIUM = 'md',
+  LARGE = 'lg'
+}
